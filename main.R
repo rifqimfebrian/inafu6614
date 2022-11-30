8942d8b7156e5e59c6212740dd57b00c4a476cfa
Rifqi Febrian <67940308+rifqifebrian@users.noreply.github.com>
2022-11-29 21:09:54 -0500

    updating the outline

--- main.R
+++ main.R
@@ -14,11 +14,52 @@ library(readxl)
 #get directory
 getwd()
 
+################################################################################
+#DATA UPLOADING
 #open census data
 uhi_us <- read_csv("Census_UHI_US_Urbanized_recalculated.csv")
 tract_income <- read_csv("ACSST5Y2017.S1902-Data.csv")
 tract_demog <- read_csv("ACSST5Y2017.S0101-Data.csv")
 
+################################################################################
+#DATA CLEANSING
+#renaming column for age and sex
 
 
-#
+#renaming column for income
+
+
+#renaming column for race
+
+
+#joining age, sex, income, and race
+
+
+#filter necessary UHI variable
+
+
+#joining UHI with age, sex, income, and race
+
+################################################################################
+#DATA EXPLORATORY
+
+#Creating a table between UHI and demographic + socioeconomic
+
+################################################################################
+#DATA ANALYSIS
+
+#grouping to lower income and higher income
+
+#understanding the differences in races
+
+#understanding the differences in age
+
+#understanding the differences in sex
+
+################################################################################
+#DATA MAPPING
+
+
+
+################################################################################
+#DATA CONCLUSION
